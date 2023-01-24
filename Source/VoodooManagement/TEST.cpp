a5cb065a543fe4a0146fae41853d6f1a94dfea2c
junkaichen <junkaichenalex@gmail.com>
2023-01-24 10:53:54 -0700

    SETUP

---
+++ Source/VoodooManagement/TEST.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TEST.h"
+
+// Sets default values
+ATEST::ATEST()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATEST::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATEST::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
